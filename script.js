f94bd9d04c434ddad218bcb3bdf5f183076d0b5a
Helora <helora.cordeiro@escola.pr.gov.br>
2025-03-24 17:33:04 -0300

    XD

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 const result = document.querySelector(".result");
-const buttons = document.querySelectorAll(".buttons");
+const buttons = document.querySelectorAll(".buttons button");
+
+let currentNumber = "";
+let firstOperand = null;
+let operator = null;
+let restart = false;
+
+function update_result(originClear = false) {
+    result.innerText = originClear ? 0 : currentNumber.replace(".", ",");
+}
+
+function addDigit(digit) {
+    if(digit === "," && (currentNumber.includes(",") || !currentNumber))
+        return;
+
+    if (restart) {
+        currentNumber = digit;
+        restart = false;
+    } else {
+        currentNumber += digit;
+    }
+
+    update_result();
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+const buttonText = button.innerText;
+if (/^[0-9,]+$/.test(buttonText)) {
+    addDigit(buttonText);
+}
+    });
+});
